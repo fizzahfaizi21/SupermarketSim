40491af013f044d0b6cf06b1d823f25d3d346e76
fizzahfaizi21 <fizzahfaizi21@gmail.com>
2026-02-22 07:37:05 -0500

    added game engine (game loop) code and a diagrams folder

---
+++ game/game_engine.py
@@ -0,0 +1,9 @@
+from game.simulation import Simulation
+
+class GameEngine:
+    def __init__(self):
+        self.simulation = Simulation()
+
+    def run(self):
+        while self.simulation.is_running():
+            self.simulation.update()
