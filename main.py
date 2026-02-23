1e35f4f4e0db6a9a66b1517584e5991a22293941
fizzahfaizi21 <fizzahfaizi21@gmail.com>
2026-02-23 03:57:06 -0500

    Integrated user management with game flow

--- main.py
+++ main.py
@@ -9,3 +9,25 @@ def main():
 if __name__ == "__main__":
     main()
 
+from ui.console_ui import registration_menu, login_menu, logout_menu
+from systems.session import get_current_user
+
+while True:
+    if not get_current_user():
+        print("\n1. Register\n2. Login\n3. Exit")
+        choice = input("> ")
+
+        if choice == "1":
+            registration_menu()
+        elif choice == "2":
+            login_menu()
+        else:
+            break
+    else:
+        print("\n1. Play Game\n2. Logout")
+        choice = input("> ")
+
+        if choice == "1":
+            print("Game starting...")
+        elif choice == "2":
+            logout_menu()
